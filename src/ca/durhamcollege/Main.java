cb0b829873ca8ae68a95199779880884f499ca7e
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-12 03:38:39 -0500

    Added Input and Validation

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -7,6 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //VARIABLE DECLARATION
+        Scanner keyboard = new Scanner(System.in);
+        int games = 2;
+        int people = 3;
+        int[][] bowling = new int[people][games];
+        String[] names = {"Fleur's", "Gabriel's", "Thom's"};
+        boolean isValidInput = false;
+
+        System.out.println("BOWLING SCORES");
+        //PROCESSING
+        while (!isValidInput)
+        {
+            try
+            {
+                for (int x = 0; x < games; x++)
+                {
+                    for (int y = 0; y < people; y++)
+                    {
+                        System.out.print("Please enter " + names[y] + " score for GAME #" + (x + 1) + ": ");
+                        bowling[y][x] = keyboard.nextInt();
+                        isValidInput = true;
+                    }
+                }
+
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must entered an invalid number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+
+        }
     }
 }
 
