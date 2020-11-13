541183358b5adde20477b9297ea75c56e4d60641
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-12 23:45:43 -0500

    Added Range Validation and Output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -2,44 +2,78 @@ package ca.durhamcollege;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) {
-
-        //VARIABLE DECLARATION
+    public static void main(String[] args)
+    {
+        //Variable Declarations
         Scanner keyboard = new Scanner(System.in);
         int games = 2;
         int people = 3;
         int[][] bowling = new int[people][games];
-        String[] names = {"Fleur's", "Gabriel's", "Thom's"};
+        String[] names = new String[]{"Fleur's", "Gabriel's", "Tom's"};
         boolean isValidInput = false;
+        int minScore = 0;
+        int maxScore = 300;
+        float totalScore = 0;
 
+        //Displays title for output
         System.out.println("BOWLING SCORES");
-        //PROCESSING
-        while (!isValidInput)
+
+        for (int x = 0; x < games; x++)
         {
-            try
+            for (int y = 0; y < people; y++)
             {
-                for (int x = 0; x < games; x++)
+                //Loops to validate the input entered by the user
+                do
                 {
-                    for (int y = 0; y < people; y++)
+                    //Initializes try to catch a possible error
+                    try
                     {
+                        // Prompts the user to enter the scores for the game
                         System.out.print("Please enter " + names[y] + " score for GAME #" + (x + 1) + ": ");
                         bowling[y][x] = keyboard.nextInt();
-                        isValidInput = true;
+
+
+                        // Validates if the user enter a number between 0 and 300, inclusive
+                        if (bowling[y][x] < minScore || bowling[y][x] > maxScore)
+                        {
+                            //Displays error message
+                            System.out.println("Invalid input. Value between " + minScore + " and "
+                                    + maxScore + " needed. Please try again.");
+                            isValidInput = false;
+                        }
+                        else
+                            {
+                                isValidInput = true;
+                            }
                     }
-                }
-
+                    catch(InputMismatchException inputMismatchException)
+                    {
+                        //Displays error message
+                        System.out.println("Error: You entered an invalid number. Please try again.");
+                        keyboard.nextLine();
+                        isValidInput = false;
+                    }
+                }while(!isValidInput);
             }
-            catch (InputMismatchException inputMismatchException)
+        }
+
+        //Output
+        for(int i = 0; i < people; i++)
+        {
+            System.out.println("");
+            System.out.println("Score Details for " + names[i] + ":");
+            for(int j = 0; j < games; j++)
             {
-                System.out.println("Error: You must entered an invalid number");
-                keyboard.nextLine();
-                isValidInput = false;
+                System.out.println("Game # " + j + ": " + bowling[i][j]);
+                totalScore += bowling[i][j];
             }
-
+            // Average
+            float average = totalScore / games;
+            //Displays average and sets totalScore back to zero
+            System.out.println("Average for " + names[i] + ": " + average);
+            totalScore = 0;
         }
     }
 }
-
