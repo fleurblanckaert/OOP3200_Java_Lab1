e2984cf3e193457381c93c1748700f656eef2e12
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-12 23:57:58 -0500

    Added Comments

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,3 +1,10 @@
+/**
+ * @Author: Gabriel Dietrich (100764733), Fleur Blanckaert (100747083)
+ * @Date: Nov 12 2020
+ * @Description: This application prompts the user to enter the score of 2 bowling
+ *               games for a total of 3 people and then calculates the average
+ *               score for each player
+ */
 package ca.durhamcollege;
 
 import java.util.InputMismatchException;
@@ -20,38 +27,39 @@ public class Main {
         //Displays title for output
         System.out.println("BOWLING SCORES");
 
+        //Loops through 2 dimensional array
         for (int x = 0; x < games; x++)
         {
             for (int y = 0; y < people; y++)
             {
-                //Loops to validate the input entered by the user
+                //Loops until user enters valid input
                 do
                 {
                     //Initializes try to catch a possible error
                     try
                     {
-                        // Prompts the user to enter the scores for the game
+                        //Prompts the user to enter the scores for the game
                         System.out.print("Please enter " + names[y] + " score for GAME #" + (x + 1) + ": ");
                         bowling[y][x] = keyboard.nextInt();
 
 
-                        // Validates if the user enter a number between 0 and 300, inclusive
+                        //Validates if the user enter a number between 0 and 300, inclusive
                         if (bowling[y][x] < minScore || bowling[y][x] > maxScore)
                         {
                             //Displays error message
-                            System.out.println("Invalid input. Value between " + minScore + " and "
-                                    + maxScore + " needed. Please try again.");
+                            System.out.println("\nError: You entered an invalid range. Value between " + minScore + " and "
+                                    + maxScore + " needed. Please try again.\n");
                             isValidInput = false;
                         }
                         else
-                            {
-                                isValidInput = true;
-                            }
+                        {
+                            isValidInput = true;
+                        }
                     }
                     catch(InputMismatchException inputMismatchException)
                     {
                         //Displays error message
-                        System.out.println("Error: You entered an invalid number. Please try again.");
+                        System.out.println("\nError: You entered an invalid number. Please try again.\n");
                         keyboard.nextLine();
                         isValidInput = false;
                     }
@@ -69,8 +77,10 @@ public class Main {
                 System.out.println("Game # " + j + ": " + bowling[i][j]);
                 totalScore += bowling[i][j];
             }
-            // Average
+
+            //Calculates Average
             float average = totalScore / games;
+
             //Displays average and sets totalScore back to zero
             System.out.println("Average for " + names[i] + ": " + average);
             totalScore = 0;
